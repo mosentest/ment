c12943d2e182601e101739a693d9f339696115f5
Zimuu <leiyangx2000@hotmail.com>
2015-06-03 11:00:40 +0200

    implemented jms/mdb

--- mentModule/src/hemu/ment/core/mdb/TextMDB.java
+++ mentModule/src/hemu/ment/core/mdb/TextMDB.java
@@ -10,19 +10,20 @@ import javax.jms.TextMessage;
 /**
  * Created by muu on 2015/6/2.
  */
-@MessageDriven(name = "MessageMDBSample", activationConfig = {
+@MessageDriven(mappedName = "queue/test", name="textQueue", activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/test"),
-        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
-public class TestMDB implements MessageListener {
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+        @ActivationConfigProperty(propertyName = "user", propertyValue = "admin"),
+        @ActivationConfigProperty(propertyName = "password", propertyValue = "123456y!")})
+public class TextMDB implements MessageListener {
 
+    @Override
     public void onMessage(Message message) {
-
         TextMessage tm = (TextMessage) message;
         try {
             System.out.println("Received message " + tm.getText());
         } catch (JMSException e) {
-
             e.printStackTrace();
         }
 
--- mentModule/test/Client.java
+++ mentModule/test/Client.java
@@ -6,31 +6,36 @@ import java.util.Properties;
 /**
  * Created by muu on 2015/6/2.
  */
-public class Client
-{
-    public static void main(String[] args) throws Exception
-    {
-        QueueConnection cnn = null;
-        QueueSender sender = null;
-        QueueSession session = null;
+public class Client {
+
+    private static final String CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    private static final String DESTINATION = "queue/test";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+    private static final String PROVIDER_URL = "http-remoting://127.0.0.1:80";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "123456y!";
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = null;
+        MessageProducer producer = null;
+        Session session = null;
         Properties prop = new Properties();
-        prop.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.jboss.naming.remote.client.InitialContextFactory");
+        prop.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
         prop.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        prop.put(Context.PROVIDER_URL, "remote://localhost:4447");
-        prop.put(Context.SECURITY_PRINCIPAL, "muu");
-        prop.put(Context.SECURITY_CREDENTIALS, "Hyrac0therium_");
+        prop.put(Context.PROVIDER_URL, PROVIDER_URL);
+        prop.put(Context.SECURITY_PRINCIPAL, USERNAME);
+        prop.put(Context.SECURITY_CREDENTIALS, PASSWORD);
         prop.put("jboss.naming.client.ejb.context", true);
         InitialContext ctx = new InitialContext(prop);
-        Queue queue = (Queue) ctx.lookup("queue/test");
-        QueueConnectionFactory factory = (QueueConnectionFactory) ctx.lookup("java:jboss/exported/jms/RemoteConnectionFactory");
-        cnn = factory.createQueueConnection();
-        session = cnn.createQueueSession(false, QueueSession.AUTO_ACKNOWLEDGE);
 
-        TextMessage msg = session.createTextMessage("Hello World");
-
-        sender = session.createSender(queue);
-        sender.send(msg);
+        QueueConnectionFactory factory = (QueueConnectionFactory) ctx.lookup(CONNECTION_FACTORY);
+        Destination destination = (Destination) ctx.lookup(DESTINATION);
+        connection = factory.createConnection(USERNAME, PASSWORD);
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        producer = session.createProducer(destination);
+        connection.start();
+        TextMessage message = session.createTextMessage("Hello world");
+        producer.send(message);
         System.out.println("Message sent successfully to remote queue.");
 
     }
