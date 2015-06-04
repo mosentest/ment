ead8c5bb273a22cc06030e65c7f31f2aef36a449
Zimuu <leiyangx2000@hotmail.com>
2015-06-04 21:15:16 +0200

    split module

---
+++ commModule/src/hemu/ment/communication/ejb/ChatEJB.java
@@ -0,0 +1,7 @@
+package hemu.ment.communication.ejb;
+
+/**
+ * Created by muu on 2015/6/4.
+ */
+public class ChatEJB {
+}
---
+++ commModule/src/hemu/ment/communication/entity/ChatMessage.java
@@ -0,0 +1,19 @@
+package hemu.ment.communication.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by muu on 2015/6/4.
+ */
+@Entity
+@Table(name = "chat_message", schema = "ment_communication")
+public class ChatMessage implements Serializable {
+
+    private static final long serialVersionUID = 7325167214419439874L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
